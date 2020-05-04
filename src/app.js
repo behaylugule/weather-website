ae663041f9f291a6e082173ddf2bbca74cb7f694
unknown <Behaylu Gule>
2020-05-05 00:36:27 +0300

    error handle

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
     //define path for express config
 const publicDircteryPath = path.join(__dirname, '../public')
 const templatePath = path.join(__dirname, '../templates/views')
